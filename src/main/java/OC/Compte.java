2689f2d518b6d4a4783a2c2339aaba08b8e6fd81
salmaboudehane1 <salma.solo0204@gmail.com>
2024-10-13 13:15:42 +0000

    Update Compte.java
    Rendre classe Compte abstract

--- src/main/java/OC/Compte.java
+++ src/main/java/OC/Compte.java
@@ -2,53 +2,29 @@ package OC;
 
 import java.util.ArrayList;
 
-public class Compte {
-    private ArrayList<Operation> operations;
-    private String id;
-    private String dateCreation;
-    private double montant;
-    private TypeCompte typeCompte;
+public abstract class Compte {
+	protected ArrayList<Operation> operations;
+    protected String id;
+    protected String dateCreation;
+    protected double montant;
 
-    public Compte(String id, String dateCreation, TypeCompte typeCompte) {
+    public Compte(String id, String dateCreation) {
         this.id = id;
         this.dateCreation = dateCreation;
-        this.typeCompte = typeCompte;
         this.operations = new ArrayList<>();
         this.montant = 0;
     }
-    public boolean addOperation(Operation operation){
-        if(this.typeCompte == TypeCompte.Courant){
-            if(operation.getTypeOperation() == TypeOperation.Debit){
-                this.operations.add(operation);
-                this.montant+= operation.getMontant();
-                return true;
-            }
-            else{
-                if(this.montant >= operation.getMontant()){
-                    this.operations.add(operation);
-                    this.montant-= operation.getMontant();
-                    return true;
-                }
-                return false;
-            }
-        }
-        else{
-            if(operation.getTypeOperation() == TypeOperation.Debit){
-                this.operations.add(operation);
-                this.montant+= operation.getMontant()*0.05;
-                return true;
-            }
-            else{
-                if(this.montant >= operation.getMontant()*0.1 &&
-                        java.time.LocalDate.now().getYear() > Integer.parseInt(this.dateCreation.split("/")[2]))
-                {
-                    this.operations.add(operation);
-                    this.montant-= operation.getMontant()*0.1;
-                    return true;
-                }
-                return false;
-            }
-        }
 
+    public void addOperation(Operation operation) {
+        operation.execute(this);
+    	this.operations.add(operation);
+    }
+    
+    public abstract boolean augmenterMontant(double montant); 
+
+    public abstract boolean diminuerMontant(double montant);
+    
+    public double getMontant() {
+        return montant;
     }
 }
